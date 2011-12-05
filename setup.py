7d1b42d52afb8e8d7e34552d0630ac0ee998697d
Thibaut Girka <thib@sitedethib.com>
2011-12-05 23:57:58 +0100

    Minor changes to the setup script

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 # -*- encoding: utf-8 -*-
 
 import os, sys
-#import shutil
 from distutils.core import setup
 from distutils.extension import Extension
 
+
+# Cython is needed
 try:
     from Cython.Distutils import build_ext
 except ImportError:
     print('You don’t seem to have Cython installed. Please get a '
           'copy from www.cython.org and install it')
     sys.exit(1)
+
 
 packages = []
 extensions = []
@@ -20,14 +22,18 @@ for directory, _, files in os.walk('pytouhou'):
     packages.append(package)
     for filename in files:
         if filename.endswith('.pyx'):
-            extensions.append(Extension('%s.%s' % (package, filename[:-4]),
-                                        ['%s/%s' % (directory, filename)]))
+            extension_name = '%s.%s' % (package, os.path.splitext(filename)[0])
+            extensions.append(Extension(extension_name,
+                                        [os.path.join(directory, filename)]))
 
-#TODO: put eclviewer.py in /usr/bin/ the right way
-#shutil.copyfile('eclviewer.py', 'pytouhou/pytouhou')
 
 setup(name='PyTouhou',
+      author='Thibaut Girka',
+      author_email='thib@sitedethib.com',
+      url='http://hg.sitedethib.com/touhou/',
+      license='GPLv3',
       packages=packages,
       ext_modules=extensions,
-      #scripts=['pytouhou/pytouhou'],
-      cmdclass = {'build_ext': build_ext})
+      cmdclass = {'build_ext': build_ext}
+     )
+
